8ea6521fdcb57ce00a011c7e5cae4d16f6fcd2c9
foxcrate <ahmedmustafa.pro19@gmail.com>
2024-10-13 05:53:43 +0300

    finish

--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    // return 'Hello World!';
-    return 'true';
-  }
-}
+export class AppService {}
--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -3,18 +3,18 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.REDIS,
-      options: {
-        port: 6379,
-        host: 'localhost',
-      },
-    },
-  );
+  const app = await NestFactory.create(AppModule);
 
-  await app.listen();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      port: 6379,
+      host: 'localhost',
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3004);
   console.log('User service is listening');
 }
 bootstrap();
